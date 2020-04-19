a3afa8d26d00a3edf632051e313fe9cac7a99739
R2J <saharituraj@gmail.com>
2020-04-19 22:52:32 +0530

    Update temppredict.py

--- temppredict.py
+++ temppredict.py
@@ -2,37 +2,23 @@
 """
 Spyder Editor
 
-@author: Rituraj Saha
+@author: R2J
 
 """
 
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pickle
 
-# Importing the Chennai Dataset
-dataset=pd.read_csv("Chennai.csv")
-newdataset=dataset
-x=newdataset.iloc[:,[0,1,2]].values
-y=newdataset.iloc[:,3].values
-newdataset.dropna(inplace=True) 
+# Importing the Dataset
+dataset=pd.read_csv("Temperature dataset\Chennai.csv")
+dataset=pd.read_csv("Temperature dataset\Delhi.csv")
+dataset=pd.read_csv("Temperature dataset\Mumbai.csv")
+dataset=pd.read_csv("Temperature dataset\Kolkata.csv")
 
-# Importing the Delhi Dataset
-dataset=pd.read_csv("Delhi.csv")
-newdataset=dataset
-x=newdataset.iloc[:,[0,1,2]].values
-y=newdataset.iloc[:,3].values
-newdataset.dropna(inplace=True) 
 
-# Importing the Mumbai Dataset
-dataset=pd.read_csv("Mumbai.csv")
-newdataset=dataset
-x=newdataset.iloc[:,[0,1,2]].values
-y=newdataset.iloc[:,3].values
-newdataset.dropna(inplace=True) 
-
-# Importing the Kolkata Dataset
-dataset=pd.read_csv("Kolkata.csv")
 newdataset=dataset
 x=newdataset.iloc[:,[0,1,2]].values
 y=newdataset.iloc[:,3].values
@@ -49,33 +35,11 @@ from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators=2000,random_state=0)
 regressor.fit(x_train, y_train)
 
-# Fitting Model to the Training set(SVR)
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
-regressor.fit(x_train, y_train)
-
-# Fitting Model to the Training set(Multiple Linear Regression)
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(x_train, y_train)
-
-# Fitting Model to the Training set(Polynomial Regression)
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-poly_reg = PolynomialFeatures(degree = 3)
-X_poly = poly_reg.fit_transform(x_train)
-poly_reg.fit(X_poly, y_train)
-lin_reg = LinearRegression()
-lin_reg.fit(X_poly, y_train)
-
 
 # Predicting the Test set results
 y_pred = regressor.predict(x_test)
 
+
 # Analyzing the outut
 from sklearn.metrics import r2_score  
 print("r2_score: ",r2_score(y_test, y_pred)) 
-
-
-
-
